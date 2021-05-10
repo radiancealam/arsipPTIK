376330c5b3bb9d6234e8410d4b98568360284856
radiancealam <pratamaradiance7@gmail.com>
2021-05-10 10:44:33 +0700

    install template sb admin 2

---
+++ app/Views/index.php
@@ -0,0 +1,9 @@
+<?= $this->extend('templates/main'); ?>
+
+<?= $this->section('content'); ?>
+<h1 class="h3 mb-4 text-gray-800">Selamat Datang di Arsip PTIK</h1>
+
+
+
+
+<?= $this->endSection(); ?>
---
+++ app/Views/templates/_includes/sidebar.php
@@ -0,0 +1,67 @@
+<ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-book"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Arsip PTIK</div>
+    </a>
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Main Dashboard
+    </div>
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Manajemen
+    </div>
+
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-users"></i>
+            <span>Kelola User</span></a>
+    </li>
+
+    <!-- Nav Item - Tables -->
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Kelola Dokumen</span></a>
+    </li>
+
+    <!-- Nav Item - Tables -->
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-book-reader"></i>
+            <span>Kelola Pinjaman</span></a>
+    </li>
+
+    <!-- Nav Item - Tables -->
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-dollar-sign"></i>
+            <span>Kelola Denda</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
